a94d58b3ddee3aa06051eda697b434dc047fe0ec
yangzhipeng <yzp0719@126.com>
2022-09-08 18:01:56 +0800

    整合Nacos

--- src/main/java/com/yzp/RunApp.java
+++ src/main/java/com/yzp/RunApp.java
@@ -1,13 +1,15 @@
 package com.yzp;
 
+import cn.hutool.core.date.DateUtil;
+import cn.hutool.core.date.TimeInterval;
+import com.alibaba.nacos.api.annotation.NacosProperties;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
-
-import java.time.Instant;
 
 /**
  * 启动类
@@ -20,12 +22,14 @@ import java.time.Instant;
 @EnableScheduling
 @Configuration
 @Slf4j
+@EnableDiscoveryClient
 public class RunApp {
 
     public static void main(String[] args) {
+        TimeInterval timeInterval = DateUtil.timer();
         SpringApplication.run(RunApp.class, args);
         if (log.isInfoEnabled()) {
-            log.info(">>>>>>>>本次启动时间[{}]<<<<<<<<", Instant.now());
+            log.info(">>>>>>>>本次启动耗时[{}]毫秒<<<<<<<<", timeInterval.interval());
         }
     }
 }
--- src/main/java/com/yzp/utils/map/QQMapUtil.java
+++ src/main/java/com/yzp/utils/map/QQMapUtil.java
@@ -4,12 +4,9 @@ import cn.hutool.crypto.SecureUtil;
 import cn.hutool.json.JSONObject;
 import cn.hutool.json.JSONUtil;
 import com.yzp.utils.client.OkHttpUtil;
-import com.yzp.utils.client.WebClientHttpUtils;
 import lombok.experimental.UtilityClass;
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * QQMapUtil
